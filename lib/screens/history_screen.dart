6b0b33afa49297925f7bb4f9c72ab3e2a26eb1d6
Mst.Sadia Siddika <157734253+SadiaSiddika@users.noreply.github.com>
2025-12-16 12:59:31 +0600

    Sadia_3

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,82 +1,65 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/game_provider.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final historyRef = FirebaseFirestore.instance.collection("matches");
+    final game = context.watch<GameProvider>();
+    final history = game.history;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Match History"),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.delete_forever),
-            onPressed: () async {
-              // DELETE ALL HISTORY
-              final docs = await historyRef.get();
-              for (var doc in docs.docs) {
-                await doc.reference.delete();
-              }
-
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text("All history cleared")),
-              );
-            },
-          ),
-        ],
-      ),
-
-      body: StreamBuilder(
-        stream: historyRef.orderBy("timestamp", descending: true).snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.data!.docs.isEmpty) {
-            return const Center(
+      appBar: AppBar(title: const Text("Match History"), centerTitle: true),
+      body: history.isEmpty
+          ? const Center(
               child: Text(
-                "No match history yet",
+                "No matches played yet",
                 style: TextStyle(fontSize: 18),
               ),
-            );
-          }
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: history.length,
+              itemBuilder: (context, index) {
+                final match = history[index];
+                final DateTime time = match["time"];
+                //final String result = match["result"];
+                final String symbol = match["symbol"];
+                final String winner = match["winner"];
 
-          final matches = snapshot.data!.docs;
+                return Card(
+                  child: ListTile(
+                    //leading: Icon(
+                    //result == "X"
+                    // ? Icons.close
+                    //: result == "O"
+                    //? Icons.circle_outlined
+                    //: Icons.handshake,
+                    // ),
+                    //title: Text(
+                    // result == "Tie" ? "Match Tied" : "Winner: $result",
+                    // ),
+                    leading: Icon(
+                      symbol == "X"
+                          ? Icons.close
+                          : symbol == "O"
+                          ? Icons.circle_outlined
+                          : Icons.handshake,
+                    ),
+                    title: Text(
+                      winner == "Tie" ? "Match Tied" : "Winner: $winner",
+                    ),
 
-          return ListView.builder(
-            itemCount: matches.length,
-            itemBuilder: (context, index) {
-              final data = matches[index].data();
-              final p1 = data["player1"];
-              final p2 = data["player2"];
-              final winner = data["winner"];
-              final time = (data["timestamp"] as Timestamp).toDate();
-
-              return Card(
-                margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                child: ListTile(
-                  leading: Icon(
-                    winner == "Tie"
-                        ? Icons.horizontal_rule
-                        : Icons.emoji_events,
-                    color: winner == "Tie" ? Colors.orange : Colors.green,
-                    size: 32,
+                    subtitle: Text(
+                      "${time.day}-${time.month}-${time.year}  "
+                      "${time.hour}:${time.minute.toString().padLeft(2, '0')}",
+                    ),
                   ),
-                  title: Text("$p1 vs $p2"),
-                  subtitle: Text(
-                    "Winner: $winner\n${time.toString().substring(0, 16)}",
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
+                );
+              },
+            ),
     );
   }
 }
